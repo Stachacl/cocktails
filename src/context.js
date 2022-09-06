453e41fc372ebdaeadf23710e05655c8084c90da
Stacha Clemens <stacha@MacBook-Pro-2.local>
2022-09-07 09:52:01 +1000

    setting fetch fuctionality

--- src/context.js
+++ src/context.js
@@ -5,13 +5,27 @@ const url = "https://www.thecocktaildb.com/api/json/v1/1/search.php?s=";
 const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [searchTerm, setSearchTerm] = useState("a");
   const [cocktails, setCocktails] = useState([]);
 
+const fetchDrinks = async () => {
+  setLoading (true)
+  try {
+    const response = await fetch ()
+
+  } catch (error) {
+    console.log(error)
+  }
+}
+
   return (
     <AppContext.Provider
-      value={{ loading, searchTerm, cocktails, setSearchTerm, }}
+      value={{ 
+        loading, 
+        cocktails, 
+        setSearchTerm, 
+      }}
     >
       {children}
     </AppContext.Provider>
