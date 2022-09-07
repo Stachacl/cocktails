bb78fdfc81547056ccff23446c85d253be82d3d9
Stacha Clemens <stacha@192-168-1-107.tpgi.com.au>
2022-09-07 20:07:56 +1000

    hero image added to Home.js & styling to index.css

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -5,6 +5,9 @@ import SearchForm from "../components/SearchForm";
 const Home = () => {
   return (
     <main>
+      <div className="hero-img">
+        <img src="https://ik.imagekit.io/stcl/Drinks/black-magic_Yr1asZ-fJ.jpg?ik-sdk-version=javascript-1.4.3&updatedAt=1662544788470" alt="black colour cocktail" className="hero-img"/>
+      </div>
       <SearchForm />
       <CocktailList />
     </main>
