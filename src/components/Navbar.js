e3876f2e7227bfdeea40d1b7ab222c4e5e0b0bc3
Stachacl <stacha.l@icloud.com>
2022-09-09 22:09:04 +1000

    search form functionality set up

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,28 +5,40 @@ import logo from "../logo.svg";
 const Navbar = () => {
   return (
     <nav class="navbar navbar-expand-lg">
-    
       <div class="container">
         <Link to="/" class="navbar-brand">
-          <img src={logo} alt="cocktail db logo" className="logo" />
+          <img src={logo} alt="the cocktail club est 1896" className="logo" />
         </Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarSupportedContent"
+          aria-controls="navbarSupportedContent"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
           <span class="navbar-toggler-icon"></span>
         </button>
+
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
           <div class="w-100 d-flex justify-content-end">
             <ul class="navbar-nav mb-2 mb-lg-0">
               <li class="nav-item">
-                <Link class="nav-link active" aria-current="page" to="/">Home</Link>
+                <Link class="nav-link active" aria-current="page" to="/">
+                  Home
+                </Link>
               </li>
               <li class="nav-item">
-                <Link to="/about" class="nav-link">About</Link>
+                <Link to="/about" class="nav-link">
+                  About
+                </Link>
               </li>
             </ul>
-      
+          </div>
         </div>
       </div>
-    </div>
     </nav>
   );
 };
--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -1,18 +1,33 @@
-import React from 'react'
-import { useGlobalContext } from '../context'
+import React from "react";
+import { useGlobalContext } from "../context";
 
 const SearchForm = () => {
-  const {setSearchTerm} = useGlobalContext()
+  const { setSearchTerm } = useGlobalContext();
+  const searchValue = React.useRef("");
+  const searchCocktail = () => {
+    setSearchTerm(searchValue.current.value);
+  };
   return (
     <div class="container my-5">
-      <div class="row bg-light px-5 py-4 shadow">
-        <div class="col-4"><strong>Start typing the cocktail name </strong></div>
-        <div class="col-4">input</div>
-        <div class="col-4">button</div>
-        
+      <div class="row bg-light px-5 py-3 shadow">
+        <div class="col-4">
+          <strong>
+            <em>Start typing the cocktail name </em>
+          </strong>
+        </div>
+        <div class="col-8">
+          <form class="col-8">
+            <input
+              type="text"
+              id="name"
+              ref={searchValue}
+              onChange={searchCocktail}
+            />
+          </form>
+        </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default SearchForm
+export default SearchForm;
