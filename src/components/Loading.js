eb088cdb3a95306b4b564223b9548835e8c12e13
Stachacl <stacha.l@icloud.com>
2022-09-10 21:05:10 +1000

    loading element added

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -1,10 +1,17 @@
-import React from 'react'
+import React from "react";
 
 const Loading = () => {
- return (
-  <div className="loader">
-  </div>
- )
-}
+  return (
+    <section className="container">
+      <div className="row px-5">
+        <div class="text-center">
+          <div class="spinner-border" role="status">
+            <span class="visually-hidden">Loading...</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Loading
+export default Loading;
