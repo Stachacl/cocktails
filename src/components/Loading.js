28b2c2b094b05bdd56a5b61b33c23b0fb773dcbc
Stachacl <stacha.l@icloud.com>
2022-09-11 18:13:24 +1000

    deployed on gh pages

--- src/components/Loading.js
+++ src/components/Loading.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Loading = () => {
   return (
-    <section className="container">
+    <section className="container" class="d-flex justify-content-center">
       <div className="row px-5">
           <div class="loader-glass"></div>
           <div class="item"></div>
