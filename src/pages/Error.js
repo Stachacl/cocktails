f402e8b20885b6bdd725f72fc31b57d89f360fd5
Stachacl <stacha.l@icloud.com>
2022-10-14 19:30:44 +1100

    error page icon added

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -5,12 +5,12 @@ import {GiShatteredGlass} from 'react-icons/gi';
 const Error = () => {
   return (
     <section className=" row d-flex justify-content-center ">
-      <GiShatteredGlass />
+     <GiShatteredGlass className=" h1"/> 
       <div className="container d-flex justify-content-center">
         <h4> Oops, it is an error...</h4>
       </div>
       <Link to="/" className="btn btn-secondary w-50">
-        back home
+        Press here to search for another cocktail
       </Link>
 
     </section>
