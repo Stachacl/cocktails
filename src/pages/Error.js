df1b36927297d200b93484961b0b5b4b973cc18a
Stachacl <stacha.l@icloud.com>
2022-10-14 19:13:41 +1100

    error page

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -3,14 +3,15 @@ import { Link } from "react-router-dom";
 
 const Error = () => {
   return (
-    <section className="error-page section">
-      <div className="error-container">
-        <h1> Oops, it is an error</h1>
-        <Link to="/" className="btn btn-primary">
-          back home
-        </Link>
+    <section className=" row d-flex justify-content-center " > 
+      <div className="container d-flex justify-content-center">
+        <h4> Oops, it is an error...</h4>
       </div>
+       <Link to="/" className="btn btn-secondary w-50">
+       back home
+     </Link>
     </section>
+
   );
 };
 
