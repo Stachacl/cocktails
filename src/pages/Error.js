1f1ee34a028a52da234c43d67109eeea03022afb
Stachacl <stacha.l@icloud.com>
2022-10-14 19:23:57 +1100

    error page - icons react installed

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -1,17 +1,19 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import {GiShatteredGlass} from 'react-icons/gi';
 
 const Error = () => {
   return (
-    <section className=" row d-flex justify-content-center " > 
+    <section className=" row d-flex justify-content-center ">
+      <GiShatteredGlass />
       <div className="container d-flex justify-content-center">
         <h4> Oops, it is an error...</h4>
       </div>
-       <Link to="/" className="btn btn-secondary w-50">
-       back home
-     </Link>
-    </section>
+      <Link to="/" className="btn btn-secondary w-50">
+        back home
+      </Link>
 
+    </section>
   );
 };
 
